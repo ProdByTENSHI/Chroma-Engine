eec08746d9cff72e266fafe288b48d584f59ddac
Leon Schultz <Prodbytxnshi@gmail.com>
2024-04-30 08:50:17 +0200

    Moved Random Functionality to Header File

--- Chroma_Engine/include/util/Random.h
+++ Chroma_Engine/include/util/Random.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#include <random>
+
 namespace chroma
 {
 	class Random
 	{
 	public:
-		Random();
+		Random() {}
 
 		//! Returns a Random Int between minInclusive and maxInclusive
-		static int GetInt(int minInclusive, int maxInclusive);
+		inline static int GetInt(int minInclusive, int maxInclusive)
+		{
+			std::random_device _device;
+			std::mt19937 rng(_device());
+			std::uniform_int_distribution < std::mt19937::result_type> dist(minInclusive, maxInclusive);
+
+			return dist(rng);
+		}
 	};
 }
